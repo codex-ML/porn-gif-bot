1b00fb837dfeccc4f0699de0769da7cf2da7ea62
Anish <153320703+codex-ML@users.noreply.github.com>
2024-02-10 22:56:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
-import os
 import telebot
 import requests
-import random
-from io import BytesIO
+from telebot import types
 
-bot_token = os.environ.get('BOT_TOKEN')
-bot = telebot.TeleBot(bot_token)
+# Initialize your bot token
+bot_token = '6472433409:AAH3IKMVV__PTJvG4CfNoNvUvvlEYcsmWFA'
+bot = telebot.TeleBot(bot_token, parse_mode=None)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-  bot.reply_to(
-      message,
-    "Welcome to pron Gif bot \n"
-    "This website may contain content of an adult nature. "
-    "If you are easily offended or are under the age of 18, "
-    "please exit now \n \n"
-    "usage of bot :- \n"
-    "/anal \n"
-    "/doggystyle \n"
-    "/threesome \n"
-    "/erotic \n"
-    "/cumshot \n"
-    "/blowjob \n"
-    "/masturbating \n"
-    "/bdsm \n"
-    "/cunnilingus \n"
-    "/oral-sex \n"
-    "/69 \n"
-    "/bondage"
-  )
-  
-# main function
-@bot.message_handler(commands=['anal', 'doggystyle', 'threesome', 'cumshot', 'blowjob', 'masturbating', 'bdsm', 'cunnilingus', 'oral-sex', '69', 'bondage'])  # Add more commands as needed
-def send_random_gif(message):
-    command = message.text.split('/')[1]  # Extract the command from the message
-    gif_data = get_random_gif_data(command)  # Get a random GIF data based on the command
-    if gif_data:
-        gif_url = gif_data['img_src']
-        bot.send_animation(message.chat.id, gif_url)
-    else:
-        bot.reply_to(message, f"No GIFs found for the command /{command}.")
 
-def get_random_gif_data(endpoint):
-    response = requests.get(f"https://pron-api.onrender.com/{endpoint}")
-    data = response.json()
-    return random.choice(data) if data else None
+# Define the function to handle button clicks
+@bot.message_handler(commands=["gif"])
+def send_gif(message):
+  # Request a random GIF from the API
+  api_url = 'https://gifapi-xx7w.onrender.com/random'
+  response = requests.get(api_url)
+  if response.status_code == 200:
+    gif_data = response.json()
+    gif_url = gif_data['img_src']
+    # Send the GIF to the user
+    bot.send_animation(message.chat.id, gif_url)
+  else:
+    bot.send_message(message.chat.id,
+                     "Failed to fetch GIF. Please try again later.")
 
-bot.infinity_polling()
 
-# 6161615496
-# AAFtkF661taD-vRE3nAQ3UF5ggWMD68o2tg
-# anal', 'doggystyle', 'threesome', 'erotic', 'cumshot',
-# 'blowjob', 'masturbating', 'bdsm', 'cunnilingus', 'oral-sex',
-# '69', 'bondage'
+# Define the handler for the /start command
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+  # Send a message with the command to receive a random GIF
+  markup = types.ReplyKeyboardMarkup(row_width=2)
+  btn_gif = types.KeyboardButton('/gif')
+  btn_dice = types.KeyboardButton('/dice')
+  markup.add(btn_gif, btn_dice)
+  bot.send_message(message.chat.id,
+                   "Send the /gif command to receive a random GIF.",
+                   reply_markup=markup)
+
+
+@bot.message_handler(commands=['dice'])
+def start_handler(message):
+  bot.send_dice(message.chat.id)
+
+
+# Start the bot
+bot.polling()
