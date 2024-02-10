7af3c24981c05c427555a64542a6a3e42b7a113f
Anish <153320703+codex-ML@users.noreply.github.com>
2024-02-10 22:58:47 +0530

    Update need to setup gif-api.js

--- need to setup gif-api.js
+++ need to setup gif-api.js
@@ -1,105 +1,84 @@
-const express = require('express');
-const request = require('request');
-const cheerio = require('cheerio');
-const NodeCache = require('node-cache');
+// mongodb+srv://akg:akg8894@cluster0.d6nstty.mongodb.net/?retryWrites=true&w=majority
+
+const express = require("express");
+const request = require("request");
+const cheerio = require("cheerio");
+const NodeCache = require("node-cache");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
 const cache = new NodeCache({ stdTTL: 600 }); // Cache TTL set to 10 minutes
 
 const headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
 };
 
-function getFinalRedirectedUrl(initialUrl) {
-    return new Promise((resolve, reject) => {
-        request.get({ url: initialUrl, headers, followRedirect: false }, (error, response) => {
-            if (response.statusCode === 302 || response.statusCode === 301) {
-                resolve(response.headers.location);
-            } else {
-                resolve(initialUrl);
-            }
-        });
-    });
-}
-
-function scrapeAndGenerateLinks(url) {
-    return new Promise((resolve, reject) => {
-        const cachedLinks = cache.get(url);
-        if (cachedLinks) {
-            resolve(cachedLinks);
-            return;
-        }
-
-        request.get({ url, headers }, (error, response, body) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-
-            const $ = cheerio.load(body);
-            const imgSrcs = $('img').map((_, img) => $(img).attr('data-lazy-src')).get(); // Extract data-lazy-src attribute
-
-            Promise.all(imgSrcs.map(link => getFinalRedirectedUrl(link)))
-                .then(finalRedirectedLinks => {
-                    const result = finalRedirectedLinks.map(link => ({ img_src: link }));
-                    cache.set(url, result); // Cache the result
-                    resolve(result);
-                })
-                .catch(reject);
-        });
-    });
-}
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to the GIF API!');
+mongoose.connect("mongodb+srv://akg:akg8894@cluster0.d6nstty.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// Define routes for different categories
-const categories = [
-    'anal', 'doggystyle', 'threesome', 'erotic', 'cumshot',
-    'blowjob', 'masturbating', 'bdsm', 'cunnilingus', 'oral-sex',
-    '69', 'bondage'
-];
+const ImageUrlSchema = new mongoose.Schema({
+  img_src: String,
+});
 
-categories.forEach(category => {
-    app.get(`/${category}`, (req, res) => {
-        const url = `https://www.dieoff.org/${category}-gif/`;
-        scrapeAndGenerateLinks(url)
-            .then(linksResult => {
-                res.json(linksResult);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                res.status(500).json({ error: 'An error occurred.' });
-            });
+const ImageUrl = mongoose.model("ImageUrl", ImageUrlSchema);
+
+function getFinalRedirectedUrl(initialUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(
+      { url: initialUrl, headers, followRedirect: false },
+      (error, response) => {
+        if (response.statusCode === 302 || response.statusCode === 301) {
+          resolve(response.headers.location);
+        } else {
+          resolve(initialUrl);
+        }
+      },
+    );
+  });
+}
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the GIF API!");
+});
+
+
+app.get("/delete", async (req, res) => {
+  try {
+    const collections = await mongoose.connection.db.collections();
+
+    const deletionPromises = collections.map(async (collection) => {
+      await collection.deleteMany({});
     });
+
+    await Promise.all(deletionPromises);
+
+    res.json({ message: "All documents deleted from all collections." });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
+});
+
+app.get("/random", async (req, res) => {
+  try {
+    const count = await ImageUrl.countDocuments();
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomImageUrl = await ImageUrl.findOne().skip(randomIndex);
+
+    res.json(randomImageUrl);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred." });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
-
-
-// {
-//   "name": "nodejs",
-//   "version": "1.0.0",
-//   "description": "",
-//   "main": "index.js",
-//   "scripts": {
-//     "test": "echo \"Error: no test specified\" && exit 1"
-//   },
-//   "keywords": [],
-//   "author": "",
-//   "license": "ISC",
-//   "dependencies": {
-//     "@types/node": "^18.0.6",
-//     "cheerio": "^1.0.0-rc.12",
-//     "express": "^4.18.2",
-//     "node-cache": "^5.1.2",
-//     "node-fetch": "^3.2.6",
-//     "request": "^2.88.2"
-//   }
-// }
